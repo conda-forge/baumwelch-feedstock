e9dd369d9b4e1edbe7373e561dd3ce8ed360690f
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2022-02-05 12:29:22 -0800

    osx_arm64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@
 # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 export CXXFLAGS="$(echo "${CXXFLAGS}" | sed -E 's@-std=c\+\+[^ ]+@@g') -D_LIBCPP_DISABLE_AVAILABILITY"
 
-./configure --prefix="${PREFIX}" 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+./configure --prefix="${PREFIX}"
 make -j"${CPU_COUNT}"
 make -j"${CPU_COUNT}" install
